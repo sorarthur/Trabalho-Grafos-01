55a0c2c425fc958e2f79065edee4d84b8ef99a80
Arthur Clemente <130259647+sorarthur@users.noreply.github.com>
2025-02-26 15:41:45 -0300

    Add files via upload

--- c++/permutation_adjacencyList-Arthur.cpp
+++ c++/permutation_adjacencyList-Arthur.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
+#include <set>
+#include <chrono>
 
 using namespace std;
 
@@ -9,20 +12,22 @@ class graph
 private:
     int numOfVertex;
     vector<vector<int>> adjacencyList;
+
 public:
-    graph(int vertexN){
+    graph(int vertexN)
+    {
         numOfVertex = vertexN;
         adjacencyList.resize(numOfVertex);
     }
 
-    void addEdge(int v1, int v2){
+    void addEdge(int v1, int v2)
+    {
         adjacencyList[v1].push_back(v2);
         adjacencyList[v2].push_back(v1);
     }
 
-
- 
-    void printAdjacencyList(){
+    void printAdjacencyList()
+    {
         for (int i = 0; i < numOfVertex; ++i)
         {
             cout << "Vertex " << i << " is connected to: ";
@@ -34,8 +39,10 @@ public:
         }
     }
 
-    bool hasEdge(int v1, int v2){
-        for(int neighbor : adjacencyList[v1]){
+    bool hasEdge(int v1, int v2)
+    {
+        for (int neighbor : adjacencyList[v1])
+        {
             if (neighbor == v2)
             {
                 return true;
@@ -44,21 +51,50 @@ public:
         return false;
     }
 
-    void allCycles(){
+    string normalizeCycle(string cycle){
+        string normalized = cycle;
+        string temp = cycle;
+
+        for (size_t i = 0; i < cycle.size(); i++)
+        {
+            rotate(temp.begin(), temp.begin() + 1, temp.end());
+            if (temp < normalized)
+            {
+                normalized = temp;
+            }
+        }
+
+        reverse(temp.begin(), temp.end());
+        for (size_t i = 0; i < cycle.size(); i++)
+        {
+            rotate(temp.begin(), temp.begin() + 1, temp.end());
+            if (temp < normalized)
+            {
+                normalized = temp;
+            }
+        }
+        return normalized;
+    }
+
+    void allCycles()
+    {
         vector<int> vertices(numOfVertex);
+        set <string> cycles;
+
+        auto start = chrono::high_resolution_clock::now();
 
         for (size_t i = 0; i < numOfVertex; i++)
         {
             vertices[i] = i;
         }
-        
+
         do
         {
             bool isValid = true; // verify if has edge between vi and vi+1
 
-            for (size_t i = 0; i < numOfVertex-1; i++)
+            for (size_t i = 0; i < numOfVertex - 1; i++)
             {
-                if (!hasEdge(vertices[i], vertices[i+1]))
+                if (!hasEdge(vertices[i], vertices[i + 1]))
                 {
                     isValid = false;
                     break;
@@ -68,28 +104,45 @@ public:
             if (isValid)
             {
                 for (size_t i = 0; i < numOfVertex; i++)
-                {                
+                {
                     if (i >= 2)
                     {
                         // verify if the last of the sub-graph is the
                         // first of the graph, when it's true, the sub-graph is a cycle
                         if (hasEdge(vertices[i], vertices[0]))
                         {
+                            string temp;
                             for (size_t j = 0; j <= i; j++)
                             {
-                                printf("%c ", 'A' + vertices[j]);
+                                // printf("%c ", 'A' + vertices[j]);
+                                temp += ('A' + vertices[j]);
                             }
-                            cout << endl;
+                            string normalizedCycle = normalizeCycle(temp);
+                            cycles.insert(normalizedCycle);
+                            // cout << endl;
                         }
                     }
                 }
             }
         } while (next_permutation(vertices.begin(), vertices.end()));
-    }
 
+        int z = 0;
+        for (const string& cycle : cycles)
+        {
+            z++;
+            cout << cycle << endl;
+        }
+        
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
+
+        cout << "Total Cycles: " << z << endl;
+        cout << "Execution Time: " << duration.count() << " nanoseconds" << endl;
+    }
 };
 
-int main(void){
+int main(void)
+{
     graph G(6);
 
     /* A - 0; B - 1; C - 2; D - 3; E - 4; F - 5 */
